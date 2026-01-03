87592a6ad61a7ff16a0f084b15b16e16294f55af
Yash Patil <yashpa7083@gmail.com>
2026-01-03 23:29:20 +0530

    Fix start.sh: use python instead of python3

--- start.sh
+++ start.sh
@@ -3,7 +3,4 @@ set -e
 
 cd backend
 
-python -m pip install --upgrade pip
-pip install -r requirements.txt
-
-python -m uvicorn app.main:app --host 0.0.0.0 --port $PORT
+exec uvicorn app.main:app --host 0.0.0.0 --port $PORT
