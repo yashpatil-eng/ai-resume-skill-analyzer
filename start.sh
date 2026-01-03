9351fef78c79c1df8ed23e627348e64fa5045033
Yash Patil <yashpa7083@gmail.com>
2026-01-03 23:14:59 +0530

    Fix Railway start command

--- start.sh
+++ start.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+set -e
+
 cd backend
-pip install -r requirements.txt
-uvicorn app.main:app --host 0.0.0.0 --port $PORT
+
+python -m pip install --upgrade pip
+python -m pip install -r requirements.txt
+
+python -m uvicorn app.main:app --host 0.0.0.0 --port $PORT
